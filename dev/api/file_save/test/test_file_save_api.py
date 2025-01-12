589945f32005a1fe59daa40c753b14b79fbe57db
Issyanofsky <161239203+Issyanofsky@users.noreply.github.com>
2025-01-12 02:09:57 +0200

    Create test_file_save_api.py

---
+++ dev/api/file_save/test/test_file_save_api.py
@@ -0,0 +1,18 @@
+import unittest
+from dev.api.file_save.file_save_api import app  # Import the  add-event_api.py Flask 
+
+class FlaskAppTestCase(unittest.TestCase):
+    
+    # Set up the test client
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True  # Enable testing mode for Flask
+
+    # Test the home route
+    def test_home(self):
+        response = self.app.get('/')  # Make a GET request to the home route
+        self.assertEqual(response.status_code, 200)  # Ensure the status code is 200 (OK)
+        self.assertEqual(response.data.decode(), "Welcome to the file_save API!")  # Ensure the response is correct
+
+if __name__ == '__main__':
+    unittest.main()
