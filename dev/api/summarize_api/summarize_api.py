5199bcd5564a1e532ff34d8545f8e415ba1469cd
Issyanofsky <161239203+Issyanofsky@users.noreply.github.com>
2025-01-13 00:25:50 +0200

    Update summarize_api.py

--- dev/api/summarize_api/summarize_api.py
+++ dev/api/summarize_api/summarize_api.py
@@ -16,13 +16,10 @@ app.url_map.strict_slashes = False
 # Enable CORS for all routes
 CORS(app)
 
-
-
 # Basic home route
 @app.route('/')
 def home():
     return "Welcome to the Summarize API!"  # or render a homepage or documentation page
-
 
 @app.route('/summarize', methods=['POST'])
 def summarize():
@@ -34,16 +31,14 @@ def summarize():
         
         if not instruction or not text:
             raise ValueError("Missing Data to summarize")
-   
-
-
+        # connect to goole API
         genai.configure(api_key=os.getenv('GOOGLE_ACCESS_TOKEN'))
-        
+
         model=genai.GenerativeModel(model_name="gemini-1.5-flash", system_instruction=instruction)
         response = model.generate_content(text)
         # Return the file name as response
         return jsonify({'summery': response}), 200
-
+        
     except ValueError as e:
         logging.error(f"Value Error: {str(e)}")
         return jsonify({'status': 'error', 'message': str(e)}), 400
@@ -53,7 +48,6 @@ def summarize():
     except Exception as e:
         logging.error(f"Unexpected Error: {str(e)}")
         return jsonify({'status': 'error', 'message': 'Failed to add friend'}), 500
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5007, debug=True)
